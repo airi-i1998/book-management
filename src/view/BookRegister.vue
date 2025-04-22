5705bac3c969aac8485a702fcfed5dcaabfc64bf
airi-i1998 <airi.inoue@generosity.co.jp>
2025-04-22 18:41:24 +0900

    add: codeReader

--- src/view/BookRegister.vue
+++ src/view/BookRegister.vue
@@ -1,7 +1,34 @@
 <script setup lang="ts">
+import { BrowserMultiFormatReader } from '@zxing/browser'
+
+const codeReader = new BrowserMultiFormatReader()
+codeReader.decodeFromVideoDevice(undefined, 'video', (result, err) => {
+  if (result) {
+    console.log(result)
+  }
+  if (err && !(err instanceof Error)) {
+    console.error(err)
+  }
+})
 </script>
+
 <template>
-<div></div>
+  <div>
+    <div class="text-center mt-10">
+      <h1>QRコード読み取り</h1>
+      <p class="mt-2">QRコードをカメラで読み取ってください。</p>
+    </div>
+    <div class="video-container">
+      <video id="video" width="800" height="500" autoplay></video>
+    </div>
+  </div>
 </template>
 <style scoped lang="scss">
+.video-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70vh;
+  background-color: #fff
+}
 </style>
